f23fce3df32febfe68809413a77a6e94ba4b35e4
Miikkatp <miikka.paronen@metropolia.fi>
2026-02-11 13:49:32 +0200

    Moduulin 3 tehtävä 1

---
+++ Mod3Teht1.py
@@ -0,0 +1,4 @@
+Kuhapituus = int(input("Anna kuhan pituus senttimetreinä: "))
+if Kuhapituus<37: print("Kuha on liian lyhyt, palauta se veteen. Kuha on ", 37-Kuhapituus ,
+"Senttimetriä liian lyhyt. ")
+else: print("Kuha on tarpeeksi pitkä, voit pitää sen. ")
